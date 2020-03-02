517be5bc9780e6d09bd957278b356a7f0763b503
Joey Langley <joemagnum1@hotmail.com>
2020-03-02 11:58:57 -0500

    fixed missing image

--- components/PortfolioSection.vue
+++ components/PortfolioSection.vue
@@ -25,9 +25,7 @@
     </div>
     <article class="ml-auto mr-auto max-w-screen-lg mb-8">
       <div class="relative bg-gray-800">
-        <div class="h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2 portfolio-thinking background-tint">
-          <!-- <img class="w-full h-full object-cover" src="/images/joey-thinking.jpg" alt="Joey Thinking"> -->
-        </div>
+        <div class="h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2 portfolio-thinking background-tint" />
         <div class="relative max-w-screen-xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
           <div class="md:ml-auto md:w-1/2 md:pl-10">
             <div class="text-base leading-6 font-semibold uppercase tracking-wider text-gray-300">
